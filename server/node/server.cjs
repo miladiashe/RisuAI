ab3b9ba4ccae4d206662cb215af2d47c840c4959
Ahyoung Kim <dkdud0122@naver.com>
2025-11-23 23:30:16 +0900

    사람살려

--- server/node/server.cjs
+++ server/node/server.cjs
@@ -6,9 +6,9 @@ const { existsSync, mkdirSync, readFileSync, writeFileSync } = require('fs');
 const fs = require('fs/promises')
 const crypto = require('crypto')
 app.use(express.static(path.join(process.cwd(), 'dist'), {index: false}));
-app.use(express.json({ limit: '200mb' }));
-app.use(express.raw({ type: 'application/octet-stream', limit: '200mb' }));
-app.use(express.text({ limit: '100mb' }));
+app.use(express.json({ limit: '500mb' }));
+app.use(express.raw({ type: 'application/octet-stream', limit: '500mb' }));
+app.use(express.text({ limit: '500mb' }));
 const {pipeline} = require('stream/promises')
 const https = require('https');
 const sslPath = path.join(process.cwd(), 'server/node/ssl/certificate');
